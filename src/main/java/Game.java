b47527ae09d7b334e8882c63e7eaf6bf51a59d28
rubenf11 <93877345+rubenf11@users.noreply.github.com>
2022-10-01 11:41:01 +0100

    Update 2 Game

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -9,6 +11,8 @@ import java.io.IOException;
 
 public class Game {
     private Screen screen;
+    private int x = 10;
+    private int y = 10;
 
     public Game(){
 
@@ -28,12 +32,18 @@ public class Game {
     }
     private void draw() throws IOException{
         this.screen.clear();
-        this.screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        this.screen.setCharacter(x,y, TextCharacter.fromCharacter('X')[0]);
         this.screen.refresh();
+    }
 
+    private void processKey(KeyStroke key){
+        System.out.println(key);
     }
 
     public void run() throws IOException{
         draw();
+        KeyStroke key = screen.readInput();
+        processKey(key);
     }
+
 }
