a60980d17ad618954ce934833bcc2bda83f3e36b
rubenf11 <93877345+rubenf11@users.noreply.github.com>
2022-10-01 11:56:09 +0100

    Updated Application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,21 +8,12 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class Application {
-    public static void main(String[] args) throws IOException {
-        try {
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            Screen screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null); // we don't need a cursor
-            screen.startScreen(); // screens must be started
-            screen.doResizeIfNecessary(); // resize screen if necessary
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void main(String[] args){
+        Game jogo = new Game();
+        try{
+            jogo.run();
+        }catch (IOException e){
+            throw new RuntimeException(e);
         }
-
-        TerminalSize terminalSize = new TerminalSize(40, 20);
-        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
-        Terminal terminal = terminalFactory.createTerminal();
-
-
     }
 }
