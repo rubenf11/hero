03b3752887999257cf1387198b000ef5a4231dad
rubenf11 <93877345+rubenf11@users.noreply.github.com>
2022-10-04 17:24:04 +0100

    Updated Arena class

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -1,4 +1,8 @@
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
@@ -10,7 +14,7 @@ public class Arena {
     private Hero hero = new Hero(10,10);
     private Screen screen;
 
-    private Position position;
+    private Position position = new Position(hero.get_x(), hero.get_y());
 
     private int width;
     private int height;
@@ -20,10 +24,15 @@ public class Arena {
         this.height = height;
     }
 
-    public void draw(Screen screen) throws IOException {
-        hero.draw(screen);
-    }
+    public void draw(TextGraphics graphics) throws IOException {
 
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#336699"));
+        graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
+        graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width * 2, height * 2), ' ');
+        graphics.putString(new TerminalPosition(position.get_x() * 2, position.get_y() * 2), "\\/");
+        graphics.putString(new TerminalPosition(position.get_x() * 2, position.get_y() * 2 + 1), "/\\");
+        hero.draw(graphics);
+    }
     public void processKey(KeyStroke key) throws IOException{
         if(key.getKeyType() == KeyType.ArrowUp){
             moveHero(hero.moveUp());
