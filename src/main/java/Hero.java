94f210368697a1bb0f7e4268e58c544aa89ed16d
rubenf11 <93877345+rubenf11@users.noreply.github.com>
2022-10-04 10:29:02 +0100

    Updated Hero class

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,4 +1,8 @@
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.screen.Screen;
 
 import java.io.IOException;
@@ -7,32 +11,47 @@ public class Hero {
 
     private Position position;
 
-    public Hero(int x, int y){
-        this.position = new Position(x,y);
+    public Hero(int x, int y) {
+        this.position = new Position(x, y);
     }
 
-    public void setPosition(Position position){
+    public int get_x(){
+        return position.get_x();
+    }
+    public int get_y(){
+        return position.get_y();
+    }
+
+    public void setPosition(Position position) {
 
         this.position.set_x(position.get_x());
         this.position.set_y(position.get_y());
 
     }
 
-    public Position moveUp(){
+    public Position moveUp() {
         return new Position(position.get_x(), position.get_y() - 1);
     }
+
     public Position moveDown() {
         return new Position(position.get_x(), position.get_y() + 1);
     }
+
     public Position moveLeft() {
         return new Position(position.get_x() - 1, position.get_y());
     }
+
     public Position moveRight() {
         return new Position(position.get_x() + 1, position.get_y());
     }
 
-    public void draw(Screen screen) throws IOException {
-        screen.setCharacter(position.get_x(), position.get_y(), TextCharacter.fromCharacter('X')[0]);
+    public void draw(TextGraphics graphics) throws IOException{
+        graphics.setForegroundColor(TextColor.Factory.fromString("#FFFF33"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(position.get_x(), position.get_y()), "X");
+        graphics.setCharacter(position.get_x(), position.get_y(), TextCharacter.fromCharacter('X')[0]);
+        graphics.putString(new TerminalPosition(position.get_x() * 2, position.get_y() * 2), "\\/");
+        graphics.putString(new TerminalPosition(position.get_x() * 2, position.get_y() * 2 + 1), "/\\");
     }
 
 }
