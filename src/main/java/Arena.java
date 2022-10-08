0ae49446fc56eeb6a37e8173a58cc07ffe273b97
rubenf11 <93877345+rubenf11@users.noreply.github.com>
2022-10-08 17:08:44 +0100

    Arena class

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -18,6 +18,7 @@ public class Arena {
     private Screen screen;
     private List<Wall> walls;
     private List<Coin> coins;
+    private List<Monster> monsters;
 
     private Position position = new Position(hero.getPosition().get_x(), hero.getPosition().get_y());
 
@@ -29,6 +30,15 @@ public class Arena {
         this.height = height;
         this.walls = createWalls();
         coins = createCoins();
+        monsters = createMonsters();
+    }
+
+    public int get_width(){
+        return width;
+    }
+
+    public int get_height(){
+        return height;
     }
 
     private List<Wall> createWalls() {
@@ -56,6 +66,18 @@ public class Arena {
         return coins;
     }
 
+    private List<Monster> createMonsters() {
+        Random random = new Random();
+        ArrayList<Monster> monsters = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Monster newmonster = new Monster(random.nextInt(width - 2) + 1, random.nextInt(height - 2) + 1);
+            if (!monsters.contains(newmonster) && !newmonster.getPosition().equals(hero.getPosition())){
+                monsters.add(newmonster);
+            }
+        }
+        return monsters;
+    }
+
     public void draw(TextGraphics graphics) throws IOException {
 
         graphics.setBackgroundColor(TextColor.Factory.fromString("#336699"));
@@ -66,6 +88,9 @@ public class Arena {
 
         for(Coin coin : coins){
             coin.draw(graphics);
+        }
+        for(Monster monster : monsters){
+            monster.draw(graphics);
         }
     }
     public void processKey(KeyStroke key) throws IOException{
@@ -110,6 +135,22 @@ public class Arena {
             }
         }
 
+    }
+
+    public void moveMonsters(){
+        for(Monster monster : monsters){
+            monster.setPosition(monster.move(this));
+        }
+    }
+
+    public boolean verifyMonsterCollisions(){
+        for(Monster monster : monsters){
+            if(monster.getPosition().equals(hero.getPosition())){
+                System.out.println("Your hero is dead!");
+                return true;
+            }
+        }
+        return false;
     }
 
 }
